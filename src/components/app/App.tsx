8b40ac4d66d06eefef6e5f38e8e4e5517474818d
caglanurdemir <nurcagla.demir@gmail.com>
2019-12-21 18:23:07 +0300

    Added JSON file

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App" />
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
 }
 
 export default App;
